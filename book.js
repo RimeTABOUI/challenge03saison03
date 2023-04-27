5aa8b323187a5450463c2598f6ea5fa225462a8c
rime <rime.taboui@oclock.school>
2023-04-27 21:05:33 +0200

    send module

---
+++ book.js
@@ -0,0 +1,47 @@
+const books = [
+    {
+        title: "The Fellowship of the Ring",
+        language: "English",
+        country: "United Kingdom",
+        author: "J.R.R. Tolkien",
+        date: "1954-07-29"
+    },
+    {
+        title: "Prelude to foundation",
+        language: "English",
+        country: "United States",
+        author: "Isaac Asimov",
+        date: "1988-11-08"
+    },
+    {
+        title: "Voyage au centre de la terre",
+        language: "Français",
+        country: "France",
+        author: "Jules Verne",
+        date: "1864-11-25"
+    },
+    {
+        title: "La nuit des temps",
+        language: "Français",
+        country: "France",
+        author: "René Barjavel",
+        date: "1968-05-20"
+    },
+    {
+        title: "Carrion Comfort",
+        language: "English",
+        country: "United States",
+        author: "Dan Simmons",
+        date: "1989-02-15"
+    }
+];
+
+function titleBooks () {
+    books.forEach(element => console.log(element.title))
+}
+
+function dateBooks() {
+    books.forEach(element => console.log(element.date))
+}
+
+module.exports = titleBooks();
